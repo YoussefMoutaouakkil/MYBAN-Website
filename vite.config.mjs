5984735a487fbbeff2ac73308603afb2631e60d8
Youssef Moutaouakkil <youssef.moutaouakkil12@gmail.com>
2024-07-10 17:30:52 +0100

    hello

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,7 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+<<<<<<< HEAD
     outDir: "dist",
+=======
+    outDir: "build",
+>>>>>>> parent of 2d286f4 (t)
   },
   plugins: [react()],
 });
