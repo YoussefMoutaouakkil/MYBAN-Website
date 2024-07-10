93f5bdacac7efd21bc3fed9af32c255d8edd5992
Youssef Moutaouakkil <youssef.moutaouakkil12@gmail.com>
2024-07-10 17:42:04 +0100

    Revert "t"

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,11 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-<<<<<<< HEAD
     outDir: "dist",
-=======
-    outDir: "build",
->>>>>>> parent of 2d286f4 (t)
   },
   plugins: [react()],
 });
